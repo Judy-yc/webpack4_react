1082f5c2597c9641d845b4c77dace581c9e393a4
Pecking <songbanghui@utimes.cc>
2019-02-02 08:59:01 +0800

    fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Index from '@Views/Index';
+import Login from '@Views/login';
 
 export default () => (
   <Router>
     <Route path="/home" component={Index} />
+    <Route path="/" component={Login} />
   </Router>
 );
---
+++ src/views/Login/Index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  componentDidMount() {
+    // do something
+  }
+
+  render() {
+    return <div>21321</div>;
+  }
+}
+export default Login;
